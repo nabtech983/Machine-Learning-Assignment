f2c5c64ae2ebd96e12c70f0eb73624b87dba080a
nabtech983 <znabeel824@gmail.com>
2019-12-06 09:19:07 -0800

    data preprocessing template modified

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -6,10 +6,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('aids.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 1].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
